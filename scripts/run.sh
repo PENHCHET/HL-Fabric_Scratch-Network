d2428ac5d159ba1d98325a97e68898b058ebdb1c
gskerry <gregory.skerry@gmail.com>
2018-03-14 19:49:42 +0000

    HL Tx config file is found via ENV variable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
 
 # Running log/script of network build commands
 
 bash ./scripts/dirs.sh
 
 cryptogen generate --output=crypto-materials --config=config_hl/orderer-peers.yaml
+
+export FABRIC_CFG_PATH=${PWD}/config_hl
+# where configtxgen will look for the configtx.yaml file
