914086707249a83b6841e16c3c5073be7f7b50b1
gskerry <gregory.skerry@gmail.com>
2018-03-14 20:01:12 +0000

    set channel name (ENV)

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,8 +8,12 @@ cryptogen generate --output=crypto-materials --config=config_hl/orderer-peers.ya
 
 export FABRIC_CFG_PATH=${PWD}/config_hl
 # where configtxgen will look for the configtx.yaml file
-# TODO feature request to specify file (allowing split into separate files e.g. for genesis vs Channels, etc)
+# TODO: feature request to specify file (allowing split into separate files e.g. for genesis vs Channels, etc)
 
 configtxgen -profile NetworkGenesis -outputBlock ./artifacts/genesis.block
 
 configtxgen -profile NetworkGenesis -inspectBlock ./artifacts/genesis.block > _docs/inspectGenesis.json
+
+export CHANNEL_NAME=openchannel # (!!!!!!!!) NO CAPITAL LETTERS
+# TODO: TBD is this intentional or bug? Did not see referenced in doc.
+echo $CHANNEL_NAME
