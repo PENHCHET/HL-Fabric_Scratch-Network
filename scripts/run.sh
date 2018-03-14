14db8fee4662606541eae12ed2d7cd0e02bf6449
gskerry <gregory.skerry@gmail.com>
2018-03-14 19:55:27 +0000

    create genesis block

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,3 +9,5 @@ cryptogen generate --output=crypto-materials --config=config_hl/orderer-peers.ya
 export FABRIC_CFG_PATH=${PWD}/config_hl
 # where configtxgen will look for the configtx.yaml file
 # TODO feature request to specify file (allowing split into separate files e.g. for genesis vs Channels, etc)
+
+configtxgen -profile NetworkGenesis -outputBlock ./artifacts/genesis.block
